aabbe7edc80a8974741fa6626e1e9af9ec910345
Nikhil Dange <nsdange@ucsd.edu>
2022-11-19 21:36:36 -0800

    fixed unit tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab 8 - Starter
+
+## Nikhil Dange
+
+1) Automated tests would be within the GitHub Action, since we want these to run every time we commit code. We do not want this to manually be run (since they are to be automated), and we want to incrementally test, not only at the very end.
+
+2) No, E2E is to test the functionality of how an app works on a larger scale.
+
+3) I would not use a unit test for message functionality. Message functionality seems like a far bigger feature of an application, and a unit test might fail to test things such as whether the message was recieved, or the contents were kept intact; a function on our side would only verify that a message was sent.
+
+4) I would use a unit test for this feature, since this is a smaller feature to test. A function can easily check a message before it is sent to verify whether it is 80 characters, since we are not concerned with what happens after the message is sent.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,69 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('isPhoneNumberNormal', () => {
+    expect(functions.isPhoneNumber('858-123-4567')).toBe(true);
+});
+test('isPhoneNumberNormal', () => {
+    expect(functions.isPhoneNumber('(123)-456-7890')).toBe(true);
+});
+test('isPhoneNumberBad', () => {
+    expect(functions.isPhoneNumber('49fnwfarfa4234')).toBe(false);
+});
+test('isPhoneNumberBad', () => {
+    expect(functions.isPhoneNumber('12344345853934')).toBe(false);
+});
+
+test('isEmailNormal', () => {
+    expect(functions.isEmail('test@test.com')).toBe(true);
+});
+test('isEmailNormal', () => {
+    expect(functions.isEmail('test47@test.com')).toBe(true);
+});
+test('isEmailBad', () => {
+    expect(functions.isEmail('2374524352')).toBe(false);
+});
+test('isEmailBad', () => {
+    expect(functions.isEmail('Test@@@com')).toBe(false);
+});
+
+test('isStrongPasswordNormal', () => {
+    expect(functions.isStrongPassword('letter___12')).toBe(true);
+});
+test('isStrongPasswordNormal', () => {
+    expect(functions.isStrongPassword('letter92383')).toBe(true);
+});
+test('isStrongPasswordBad', () => {
+    expect(functions.isStrongPassword('123')).toBe(false);
+});
+test('isStrongPasswordBad', () => {
+    expect(functions.isStrongPassword('_^^3')).toBe(false);
+});
+
+test('isDateNormal', () => {
+    expect(functions.isDate('11/19/2022')).toBe(true);
+});
+test('isDateNormal', () => {
+    expect(functions.isDate('1/19/2023')).toBe(true);
+});
+test('isDateBad', () => {
+    expect(functions.isDate('December 15th, 2023')).toBe(false);
+});
+test('isDateBad', () => {
+    expect(functions.isDate('11-20-2019')).toBe(false);
+});
+
+test('isHexColorNormal', () => {
+    expect(functions.isHexColor('#955dc6')).toBe(true);
+});
+test('isHexColorNormal', () => {
+    expect(functions.isHexColor('#FCD')).toBe(true);
+});
+test('isHexColorBad', () => {
+    expect(functions.isHexColor('#092342009294')).toBe(false);
+});
+test('isHexColorBad', () => {
+    expect(functions.isHexColor('#FICI')).toBe(false);
+});
+
